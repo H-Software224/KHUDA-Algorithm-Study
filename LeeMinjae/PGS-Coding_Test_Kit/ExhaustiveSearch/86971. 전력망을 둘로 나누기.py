3ab6ce08c2855ac9a45f07a7151b55e11e18b6db
MinjaeLee <2alswo7@khu.ac.kr>
2023-11-01 23:00:53 +0900

    ✨feat: 231101_PGS_86971_전력망을 둘로 나누기 문제 풀이 추가 (재도전 실패)

--- LeeMinjae/PGS-Coding_Test_Kit/ExhaustiveSearch/86971. 전력망을 둘로 나누기.py
+++ LeeMinjae/PGS-Coding_Test_Kit/ExhaustiveSearch/86971. 전력망을 둘로 나누기.py
@@ -1,24 +1,15 @@
 def solution(n, wires):
-    answer = 0
+    ans = n
     
-    countList = []
-    count = 0
+    # wires 반복하면서 하나씩 연결 제거
+    for delete_wires in (wires[i+1:] + wires[:i] for i in range(len(wires))):
+        # 기준값은 연결 하나를 제거한 배열의 첫번째 요소
+        s = set(delete_wires[0])
+        
+        # 기준값과 교집합(&)이 있는 경우에 대해 요소를 추가
+        # 집합(set)을 사용해서 중복값 방지
+        [s.update(w) for w in delete_wires if set(w) & s]
 
-    for w in wires:
-        copy_wires = wires.copy()
-        copy_wires.remove(w)
-        print(copy_wires)
-        # copy_wires.remove(comb)
-        #setl = set([w[0]])
-        #previous = set()
-        #while (setl != previous) :
-        #    previous = setl.copy()
-        #    for wire in copy_wires.copy():
-        #        if (setl & set(wire)):
-        #            setl = setl | set(wire)
-        #countList.append(abs((n - 2*len(setl))))
-        #count = 0
-        #copy_wires = wires.copy()
-
-    #answer = min(countList)
-    #return answer
+        # 절대값을 기준으로 가장 작은 값을 고른다 = 두 전력망이 가지고 있는 송전탑 개수의 차이 (절대값)
+        ans = min(ans, abs(2 * len(s) - n))
+    return ans
